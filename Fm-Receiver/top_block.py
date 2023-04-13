ec1c4fc923eb8f440696683b552ed4418dddab79
Lekha <lekha@lekha.lekhawireless.com>
2023-04-13 16:09:45 +0530

    FM Receiver

--- Fm-Receiver/top_block.py
+++ Fm-Receiver/top_block.py
@@ -3,7 +3,7 @@
 ##################################################
 # GNU Radio Python Flow Graph
 # Title: Top Block
-# Generated: Thu Apr 13 16:04:49 2023
+# Generated: Thu Apr 13 15:59:03 2023
 ##################################################
 
 if __name__ == '__main__':
@@ -18,7 +18,7 @@ if __name__ == '__main__':
 
 from PyQt4 import Qt
 from gnuradio import analog
-from gnuradio import blocks
+from gnuradio import audio
 from gnuradio import eng_notation
 from gnuradio import filter
 from gnuradio import gr
@@ -67,30 +67,68 @@ class top_block(gr.top_block, Qt.QWidget):
         ##################################################
         # Blocks
         ##################################################
-        self.uhd_usrp_sink_0 = uhd.usrp_sink(
+        self._fm_station_range = Range(90e6, 106e6, 1, 100e6, 200)
+        self._fm_station_win = RangeWidget(self._fm_station_range, self.set_fm_station, "fm_station", "counter_slider", float)
+        self.top_layout.addWidget(self._fm_station_win)
+        self.uhd_usrp_source_0 = uhd.usrp_source(
         	",".join(("", "")),
         	uhd.stream_args(
         		cpu_format="fc32",
         		channels=range(1),
         	),
         )
-        self.uhd_usrp_sink_0.set_samp_rate(samp_rate)
-        self.uhd_usrp_sink_0.set_center_freq(95.5e6, 0)
-        self.uhd_usrp_sink_0.set_gain(80, 0)
-        self.uhd_usrp_sink_0.set_antenna("TX/RX", 0)
-        self.uhd_usrp_sink_0.set_bandwidth(1.536e6, 0)
-        self.rational_resampler_xxx_0 = filter.rational_resampler_ccc(
-                interpolation=4,
-                decimation=1,
-                taps=None,
-                fractional_bw=None,
+        self.uhd_usrp_source_0.set_samp_rate(samp_rate)
+        self.uhd_usrp_source_0.set_center_freq(fm_station, 0)
+        self.uhd_usrp_source_0.set_gain(60, 0)
+        self.uhd_usrp_source_0.set_antenna("RX2", 0)
+        self.uhd_usrp_source_0.set_bandwidth(samp_rate, 0)
+        self.qtgui_freq_sink_x_0_0 = qtgui.freq_sink_c(
+        	1024, #size
+        	firdes.WIN_BLACKMAN_hARRIS, #wintype
+        	100e6, #fc
+        	1.536e6, #bw
+        	"before filter", #name
+        	1 #number of inputs
         )
+        self.qtgui_freq_sink_x_0_0.set_update_time(0.10)
+        self.qtgui_freq_sink_x_0_0.set_y_axis(-140, 10)
+        self.qtgui_freq_sink_x_0_0.set_trigger_mode(qtgui.TRIG_MODE_FREE, 0.0, 0, "")
+        self.qtgui_freq_sink_x_0_0.enable_autoscale(True)
+        self.qtgui_freq_sink_x_0_0.enable_grid(False)
+        self.qtgui_freq_sink_x_0_0.set_fft_average(0.1)
+        self.qtgui_freq_sink_x_0_0.enable_control_panel(False)
+        
+        if not True:
+          self.qtgui_freq_sink_x_0_0.disable_legend()
+        
+        if "complex" == "float" or "complex" == "msg_float":
+          self.qtgui_freq_sink_x_0_0.set_plot_pos_half(not True)
+        
+        labels = ["", "", "", "", "",
+                  "", "", "", "", ""]
+        widths = [1, 1, 1, 1, 1,
+                  1, 1, 1, 1, 1]
+        colors = ["blue", "red", "green", "black", "cyan",
+                  "magenta", "yellow", "dark red", "dark green", "dark blue"]
+        alphas = [1.0, 1.0, 1.0, 1.0, 1.0,
+                  1.0, 1.0, 1.0, 1.0, 1.0]
+        for i in xrange(1):
+            if len(labels[i]) == 0:
+                self.qtgui_freq_sink_x_0_0.set_line_label(i, "Data {0}".format(i))
+            else:
+                self.qtgui_freq_sink_x_0_0.set_line_label(i, labels[i])
+            self.qtgui_freq_sink_x_0_0.set_line_width(i, widths[i])
+            self.qtgui_freq_sink_x_0_0.set_line_color(i, colors[i])
+            self.qtgui_freq_sink_x_0_0.set_line_alpha(i, alphas[i])
+        
+        self._qtgui_freq_sink_x_0_0_win = sip.wrapinstance(self.qtgui_freq_sink_x_0_0.pyqwidget(), Qt.QWidget)
+        self.top_layout.addWidget(self._qtgui_freq_sink_x_0_0_win)
         self.qtgui_freq_sink_x_0 = qtgui.freq_sink_c(
         	1024, #size
         	firdes.WIN_BLACKMAN_hARRIS, #wintype
-        	0, #fc
+        	100e6, #fc
         	samp_rate, #bw
-        	"", #name
+        	"after filter", #name
         	1 #number of inputs
         )
         self.qtgui_freq_sink_x_0.set_update_time(0.10)
@@ -126,27 +164,22 @@ class top_block(gr.top_block, Qt.QWidget):
         
         self._qtgui_freq_sink_x_0_win = sip.wrapinstance(self.qtgui_freq_sink_x_0.pyqwidget(), Qt.QWidget)
         self.top_layout.addWidget(self._qtgui_freq_sink_x_0_win)
-        self._fm_station_range = Range(85e6, 105e6, 100, 100e6, 200)
-        self._fm_station_win = RangeWidget(self._fm_station_range, self.set_fm_station, "fm_station", "counter_slider", float)
-        self.top_layout.addWidget(self._fm_station_win)
-        self.blocks_wavfile_source_0 = blocks.wavfile_source("/home/lekha/Downloads/re.wav", True)
-        self.blocks_add_xx_0 = blocks.add_vff(1)
-        self.analog_wfm_tx_0 = analog.wfm_tx(
-        	audio_rate=44100,
-        	quad_rate=396900,
-        	tau=75e-6,
-        	max_dev=75e3,
+        self.low_pass_filter_0 = filter.fir_filter_ccf(4, firdes.low_pass(
+        	1, 1.536e6, 96000, 15000, firdes.WIN_HAMMING, 6.76))
+        self.audio_sink_0 = audio.sink(48000, "", True)
+        self.analog_wfm_rcv_0 = analog.wfm_rcv(
+        	quad_rate=samp_rate/4,
+        	audio_decimation=8,
         )
 
         ##################################################
         # Connections
         ##################################################
-        self.connect((self.analog_wfm_tx_0, 0), (self.rational_resampler_xxx_0, 0))    
-        self.connect((self.blocks_add_xx_0, 0), (self.analog_wfm_tx_0, 0))    
-        self.connect((self.blocks_wavfile_source_0, 0), (self.blocks_add_xx_0, 0))    
-        self.connect((self.blocks_wavfile_source_0, 1), (self.blocks_add_xx_0, 1))    
-        self.connect((self.rational_resampler_xxx_0, 0), (self.qtgui_freq_sink_x_0, 0))    
-        self.connect((self.rational_resampler_xxx_0, 0), (self.uhd_usrp_sink_0, 0))    
+        self.connect((self.analog_wfm_rcv_0, 0), (self.audio_sink_0, 0))    
+        self.connect((self.low_pass_filter_0, 0), (self.analog_wfm_rcv_0, 0))    
+        self.connect((self.low_pass_filter_0, 0), (self.qtgui_freq_sink_x_0, 0))    
+        self.connect((self.uhd_usrp_source_0, 0), (self.low_pass_filter_0, 0))    
+        self.connect((self.uhd_usrp_source_0, 0), (self.qtgui_freq_sink_x_0_0, 0))    
 
     def closeEvent(self, event):
         self.settings = Qt.QSettings("GNU Radio", "top_block")
@@ -159,14 +192,16 @@ class top_block(gr.top_block, Qt.QWidget):
 
     def set_samp_rate(self, samp_rate):
         self.samp_rate = samp_rate
-        self.qtgui_freq_sink_x_0.set_frequency_range(0, self.samp_rate)
-        self.uhd_usrp_sink_0.set_samp_rate(self.samp_rate)
+        self.uhd_usrp_source_0.set_samp_rate(self.samp_rate)
+        self.uhd_usrp_source_0.set_bandwidth(self.samp_rate, 0)
+        self.qtgui_freq_sink_x_0.set_frequency_range(100e6, self.samp_rate)
 
     def get_fm_station(self):
         return self.fm_station
 
     def set_fm_station(self, fm_station):
         self.fm_station = fm_station
+        self.uhd_usrp_source_0.set_center_freq(self.fm_station, 0)
 
 
 def main(top_block_cls=top_block, options=None):
